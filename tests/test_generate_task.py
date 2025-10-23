8e056890752021dc69a068c21d6adfbbbd420439
babymerlen <mmromadin@gmail.com>
2025-10-23 18:05:34 +0300

    feat: Integrate Dishka and TaskService for test dependency injection.

--- tests/test_generate_task.py
+++ tests/test_generate_task.py
@@ -1,23 +1,24 @@
 import pytest
-from src.services.generate_task import generate_task
-
+from dishka import make_container
+from src.di.providers import ConfigProvider, LLMProvider, TaskServiceProvider
+from src.services.task_service import TaskService
 
 @pytest.mark.llm_integration
-@pytest.mark.parametrize(
-    "rarity, min_exp, max_exp, max_attr",
-    [
-        ("COMMON", 10, 20, 2),
-        ("UNCOMMON", 30, 40, 4),
-        ("RARE", 50, 60, 6),
-        ("EPIC", 70, 80, 8),
-        ("LEGENDARY", 90, 100, 10),
-    ],
-)
+@pytest.mark.parametrize("rarity, min_exp, max_exp, max_attr", [
+    ("COMMON", 10, 20, 2),
+    ("UNCOMMON", 30, 40, 4),
+    ("RARE", 50, 60, 6),
+    ("EPIC", 70, 80, 8),
+    ("LEGENDARY", 90, 100, 10)
+])
 def test_generate_task_with_all_rarities(rarity, min_exp, max_exp, max_attr):
-    task = generate_task(["MENTAL_HEALTH"], rarity)
+    container = make_container(ConfigProvider(), LLMProvider(), TaskServiceProvider())
+
+    with container() as request_container:
+        task_service = request_container.get(TaskService)
+        task = task_service.generate_task(["MENTAL_HEALTH"], rarity)
 
     assert min_exp <= task.experience <= max_exp
-
     assert task.currencyReward == task.experience // 2
 
     total_attrs = task.agility + task.strength + task.intelligence
